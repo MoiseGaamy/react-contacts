fdc1384ec9a7690bbd2eb84cba74b08a72d82238
Gamy <flokygamy@gmail.com>
2021-10-10 03:48:12 +0000

    added redux , react-redux and created store

---
+++ src/reducers/usersReducers.js
@@ -0,0 +1,19 @@
+const initialState = {
+    users: [
+        {
+            name: "jackeline",
+            email: "jackeline@gmail.com"
+        },
+        {
+            name: "pascal",
+            email: "pascal@gmail.com"
+        }
+    ]
+}
+
+const usersReducers = (state = initialState , actions) => {
+
+    return state;
+}
+
+export default usersReducers;
