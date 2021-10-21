d9988981cbcfff587440e099081c11b14c0d5ff7
Gamy <flokygamy@gmail.com>
2021-10-21 16:33:29 +0000

    implemented editContact reducer

--- src/reducers/contactReducers.js
+++ src/reducers/contactReducers.js
@@ -7,7 +7,17 @@ const usersReducers = (state = initialState , action) => {
         case 'ADD_CONTACT':
             return {
               ...state, contacts:[...state.contacts, action.payload]
-          }
+            }
+        case 'DELETE_CONTACT':
+            const deletedContat = state.contacts.filter((contact) => {
+                return contact.id !== action.payload;
+            })
+            return { ...state, contacts: deletedContat };
+        case 'EDIT_CONTACT':
+            const editedContact = state.contacts.map((contact) => {
+                return contact.id === action.payload.id ? action.payload : contact
+            })
+            return { ...state, contacts: editedContact };
         default:
             return state; 
       }
