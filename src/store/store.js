47bca4fbae379f211db87fa63bbc69a1f5cc6e69
Gamy <flokygamy@gmail.com>
2021-11-01 15:29:29 +0000

    added additonal configuration for redux store

--- src/store/store.js
+++ src/store/store.js
@@ -1,7 +1,19 @@
-import { createStore } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
 import usersReducers from "../reducers/contactReducers";
 
+import { getFirebase, reactReduxFirebase } from "react-redux-firebase";
+import { getFirestore, reduxFirestore } from "redux-firestore";
+import configObj from "../config.js";
 
-const store = createStore(usersReducers);
+
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(usersReducers, composeEnhancer(
+    applyMiddleware(
+        thunk.withExtraArgument({ getFirebase, getFirestore })),
+        reactReduxFirebase(configObj),
+        reduxFirestore(configObj)
+)
+);
 
 export default store;
