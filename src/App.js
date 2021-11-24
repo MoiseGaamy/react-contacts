4c844f4d2f8da7b0d4bcf17361183e58779bdeed
Gamy <flokygamy@gmail.com>
2021-11-24 04:24:16 +0000

    implemented react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,58 +1,23 @@
 import React,{ Component } from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Container, Row, Col } from 'react-bootstrap';
-import Users from './components/Users';
 import './App.css';
-import AddUserForm from "./components/AddUserForm";
-import { connect } from "react-redux";
-import { getAllContacts } from "./actions/contactActions.js";
+import { BrowserRouter, Route } from "react-router-dom";
+import Home from "./pages/Home.js";
+import Register from "./pages/Register.js";
+import Login from "./pages/Login.js";
+import ProtectedRoute from "./components/ProtectedRoute.js";
+
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      users: [
-        {
-          id: 1,
-          name: "ismo",
-          email: "ismo@email.com",
-          gen: 14
-       },
-        {
-          id: 2,
-          name: "stefani",
-          email: "stefani@email.com",
-          gen: 16
-       },
-       {
-          id: 3,
-          name: "jean",
-          email: "jean@email.com",
-          gen: 15
-       }
-      ]
-    }
-  }
   
-  componentDidMount() {
-    this.props.getAllContacts();
-}
   render() {
   return (
-      <>
-        <Container fluid style={{marginTop: "2rem"}}>
-          <Row>
-          <Col md="4"><AddUserForm /></Col>
-            <Col>
-              <Users usersData= {this.state.users}  />
-            </Col>
-          </Row>
-        </Container>
-      </>
+      <BrowserRouter>
+      <ProtectedRoute exact path="/" component={ Home}/>
+      <Route path="/register" component={Register}/>
+      <Route path="/login" component={Login}/>
+      </BrowserRouter>
   ); }
 }
-const mapDispatchToProps = {
-  getAllContacts,
-}
 
-export default  connect(null,mapDispatchToProps)(App);
+export default  App;
