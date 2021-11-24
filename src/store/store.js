8921f6458232c2c36e57b3dc8bbc6d23d1a48b57
Gamy <flokygamy@gmail.com>
2021-11-24 04:28:57 +0000

    modified usersReducer with combine reducer

--- src/store/store.js
+++ src/store/store.js
@@ -1,14 +1,18 @@
-import { createStore, applyMiddleware, compose } from "redux";
+import { createStore, applyMiddleware, compose , combineReducers} from "redux";
 import thunk from "redux-thunk";
 import usersReducers from "../reducers/contactReducers";
 
-import { getFirebase, reactReduxFirebase } from "react-redux-firebase";
+import { getFirebase, reactReduxFirebase, firebaseReducer } from "react-redux-firebase";
 import { getFirestore, reduxFirestore } from "redux-firestore";
 import configObj from "../config.js";
 
+const reducer = combineReducers({
+    contactState: usersReducers,
+    firebase : firebaseReducer
+})
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(usersReducers, composeEnhancer(
+const store = createStore(reducer, composeEnhancer(
     applyMiddleware(
         thunk.withExtraArgument({ getFirebase, getFirestore })),
         reactReduxFirebase(configObj),
